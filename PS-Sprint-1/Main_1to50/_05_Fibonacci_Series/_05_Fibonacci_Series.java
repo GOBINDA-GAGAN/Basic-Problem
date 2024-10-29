223110c09cb031911c91425709cee83e283794c3
Gobinda Gagan <gobindagagan@gmail.com>
2024-10-29 11:31:34 +0530

    #

--- PS-Sprint-1/Main_1to50/_05_Fibonacci_Series/_05_Fibonacci_Series.java
+++ PS-Sprint-1/Main_1to50/_05_Fibonacci_Series/_05_Fibonacci_Series.java
@@ -1,8 +1,20 @@
-
+import java.util.Scanner;
 
 public class _05_Fibonacci_Series {
      public static void main(String[] args) {
-          
+          Scanner sc = new Scanner(System.in);
+          System.out.print("Enter a Number:");
+          int num = sc.nextInt();
+          int a = 0, b = 1, c;
+
+          while (a <= num) {
+               System.out.print(a + " ");
+               
+               c = a + b;
+               a = b;
+               b = c;
+          }
+
+          sc.close();
      }
-     
 }
